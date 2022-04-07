a1dbd0531eb6439243d04161d3d7e4ff6a3b34da
padermo <fabioaeh96@gmail.com>
2022-04-07 16:15:19 -0500

    Ejercicios

--- Ejercicios-JS/Ejercicios.js
+++ Ejercicios-JS/Ejercicios.js
@@ -9,31 +9,56 @@ C -> 2
 D -> 3
 A -> 4
 ==================================================================================*/
-let corredores = {
-    A: 0,
-    B: 0,
-    C: 0,
-    D: 0,
+// let corredores = {
+//     A: 0,
+//     B: 0,
+//     C: 0,
+//     D: 0,
 
-    resultado: function(){
-        if(corredores.C > corredores.B && corredores.D > corredores.B && corredores.D > corredores.C && corredores.D < corredores.A){
-            return true;
-        }else{
-            return false;
-        }
-    },
-    intervalo: setInterval(function(){
-        corredores.A = Math.ceil(Math.random()*4);
-        corredores.B = Math.ceil(Math.random()*4);
-        corredores.C = Math.ceil(Math.random()*4);
-        corredores.D = Math.ceil(Math.random()*4);
+//     resultado: function(){
+//         if(corredores.C > corredores.B && corredores.D > corredores.B && corredores.D > corredores.C && corredores.D < corredores.A){
+//             return true;
+//         }else{
+//             return false;
+//         }
+//     },
+//     intervalo: setInterval(function(){
+//         corredores.A = Math.ceil(Math.random()*4);
+//         corredores.B = Math.ceil(Math.random()*4);
+//         corredores.C = Math.ceil(Math.random()*4);
+//         corredores.D = Math.ceil(Math.random()*4);
         
-        if(corredores.resultado()){
-            clearInterval(corredores.intervalo);
-            console.log("atleta A ", corredores.A );
-            console.log("atleta B ", corredores.B );
-            console.log("atleta C ", corredores.C );
-            console.log("atleta D ", corredores.D );
-        }
-    },10)
+//         if(corredores.resultado()){
+//             clearInterval(corredores.intervalo);
+//             console.log("atleta A ", corredores.A );
+//             console.log("atleta B ", corredores.B );
+//             console.log("atleta C ", corredores.C );
+//             console.log("atleta D ", corredores.D );
+//         }
+//     },10)
+// }
+
+/*==================================================================================
+Ejercicio 2.
+
+Hacer un programa que pida dos números y que nos diga cual es el mayor, el menor y si son iguales.
+==================================================================================*/
+
+let num1 = prompt("Escribe el primer número: ");
+let num2 = prompt("Escribe el segundo número: ");
+
+if(num1>num2){
+    console.log("El número mayor es: " + num1 + " y El número menor es: " + num2);
+}else if(num1<num2){
+    console.log("El número menor es: " + num1 + " y El número mayor es: " + num2);
+}else if(num1===num2){
+    console.log("Los dos números son iguales");
 }
+
+
+/*==================================================================================
+Ejercicio 3.
+
+Al programa anterior, si los numeros no son un número o son menores o iguales a cero, nos los vuelva a pedir
+==================================================================================*/
+
